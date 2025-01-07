dabcbcd979d35a711736ab01e9fc6d1d0534b599
Vedanteus <57452935+Vedanteus@users.noreply.github.com>
2025-01-07 17:42:09 +0530

    Test

--- Data Collection.py
+++ Data Collection.py
@@ -7,3 +7,7 @@ end_date = datetime.datetime(2023, 12, 31)
 # Fetching Case-Shiller Index data
 cs_index = web.DataReader('CSUSHPISA', 'fred', start_date, end_date)
 cs_index.to_csv('case_shiller_index.csv')  # Save locally
+
+# Fetching Federal Funds Rate data
+fed_funds_rate = web.DataReader('FEDFUNDS', 'fred', start_date, end_date)
+fed_funds_rate.to_csv('fed_funds_rate.csv')  # Save locally
